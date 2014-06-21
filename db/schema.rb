c07fbb957c4d9ffbc6ae1ae35bf7c79bbd40ee0f
Maimer <nesnl23@yahoo.com>
2014-06-21 14:02:02 -0400

    Added restaurant id column to reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140621152221) do
+ActiveRecord::Schema.define(version: 20140621175919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,10 +34,11 @@ ActiveRecord::Schema.define(version: 20140621152221) do
   end
 
   create_table "reviews", force: true do |t|
-    t.text     "description", null: false
-    t.integer  "rating",      null: false
+    t.text     "description",   null: false
+    t.integer  "rating",        null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "restaurant_id", null: false
   end
 
 end
